8894079022e469e8c8d1054fb6d0ef81925b4cc6
MarceloCabral98sp <marcelo.cabral98sp@gmail.com>
2025-10-28 03:06:31 -0300

    feat: adding initial structure of the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { OrderPage } from './features/order/pages/order-page/order-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: OrderPage }
+];
